1d0840036a022cdbb9f7d9bf4ff627fd0b18cac9
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-06-08 16:41:36 +0100

    if both players reach 20 server alternates every 2 points

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -10,13 +10,13 @@ const reducer = (state, action) => {
         case "INCREMENTPLAYER1": return{
             ...state,
             player1: player1 + 1,
-            server: (total + 1) % 5 === 0 ? !server : server,
+            server: total <= 40 ? ((total + 1) % 5 === 0 ? !server : server) : ((total + 1) % 2 === 0 ? !server : server),
             winner: (player1 + 1) >= 21 && player2 <= player1 - 1 ? "1" : ""
         };
         case "INCREMENTPLAYER2": return{
             ...state,
             player2: player2 + 1,
-            server: (total + 1) % 5 === 0 ? !server : server,
+            server: total <= 40 ? ((total + 1) % 5 === 0 ? !server : server) : ((total + 1) % 2 === 0 ? !server : server),
             winner: (player2 + 1) >= 21 && player1 <= player2 - 1 ? "2" : ""
         };
         case "RESET": return initialState;
